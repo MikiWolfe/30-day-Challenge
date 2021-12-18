bbfff44784a93360a056ddd126552a006f375918
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-18 15:00:39 -0800

    no one is going to see this anyway

---
+++ script.js
@@ -0,0 +1,30 @@
+// const output= [ 2, 5, 4, 1, 9, 12, 11, 10, 8, 7, 6, 3]
+// let randomOrder =[]
+
+// function random(){
+//     randomNumber = output[Math.floor(Math.random() * output.length)]
+//  console.log(randomNumber)
+//  for (let i = 0; i < output.length; i++) {
+//      if (randomOrder ===! randomNumber ){
+//     return true} 
+//     randomOrder.push(randomNumber)
+//  }
+
+// console.log(randomOrder)
+// }
+// random()
+
+const url = "https://www.boredapi.com/api/activity"
+function getActivity(){
+fetch(url)
+.then((res)=>
+{
+    return res.json()
+})
+.then((data) => {
+    console.log(data)
+}
+)}
+// getActivity()
+const btn = document.getElementById("btn")
+btn.addEventListener('click', getActivity)
